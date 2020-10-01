eda453384aa1d0354264365aebdf357fbb6e98c0
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-01 19:03:39 -0400

    Pass nameMenuItem and createMenuItem tests.

--- src/meal.js
+++ src/meal.js
@@ -1,11 +1,18 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name,
+    price,
+    type
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
