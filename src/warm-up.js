1f4ddf6e93c8c0a08784a47f40a2fb6a8d7bd824
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-01 18:55:27 -0400

    Complete warm up exercises

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,25 +13,39 @@
 // var type = "Dessert";
 // var inStock = 10
 
-
+let food = "Pizza";
+let type = "Meal";
+let inStock = 20;
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
+inStock *= 2;
 
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
 // Use a built in math operator to decrement the inStock count by 1.
+inStock--;
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 
-
+let isFavorite = true;
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+
+let shouldOrderMore;
+
+
+if(inStock < 20) {
+  shouldOrderMore = true;
+} else {
+  shouldOrderMore = false;
+}
+
 
 
 
@@ -42,32 +56,46 @@
 // Express the following in code: If the variable "stockCount" is less than 1, log
 // the statement 'I'm going to eat some [food]!', otherwise, log the statement 
 // 'We need to get some more [food].'
+let stockCount = 5;
 
+if (stockCount < 1) {
+  console.log(`I'm going to eat some ${food.toLowerCase()}!`);
+} else {
+  console.log(`We need to get some more ${food.toLowerCase()}.`);
+}
 
+// console.log(shouldOrderMore);
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
-
+if (isFavorite) {
+  console.log(`My favorite food is ${food.toLowerCase()}!`);
+} else {
+  console.log(`I recently ate ${food.toLowerCase()}.`);
+}
 
 //------------------------- Arrays -------------------------//
 
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
-
+let vegetables = ['tomato', 'broccoli', 'spinach', 'carrot', 'kale'];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+let elem3 = vegetables[2];
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
 
+let fruits = ['apple', 'orange', 'banana', 'cherry', 'pear', 'grape', 'nectarine']
 
 // Write the code to access the 5th element of the array, using bracket notation.
+let elem5 = fruits[4];
 
 
 //------------------------- Objects -------------------------//
@@ -76,7 +104,12 @@
 // keys: name, and isOpen. The value for name should the string 'King Soopers', and
 // the value for isOpen should be true
 
-
+let groceryStore = {
+  name: 'King Soopers',
+  isOpen: true
+}
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+groceryStore.location = 'Denver, Colorado';
