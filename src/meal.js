78c803471e646cb5e0c69483d72309e88fa4142c
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-02 18:28:59 -0400

    Pass decreasePrice tests

--- src/meal.js
+++ src/meal.js
@@ -20,13 +20,17 @@ function formatPrice(price) {
   return `$${price}`
 }
 
+function decreasePrice(price) {
+  return price * .9;
+}
+
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
   formatPrice,
-  // decreasePrice,
+  decreasePrice,
   // createRecipe
 }
 
