0b3543b288364a0bb1047ebf0710c47aacd319ec
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-04 13:52:36 -0400

    Pass restaurant - removeMenuItem tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,13 +10,41 @@ function createRestaurant(name) {
 }
 
 function addMenuItem(restaurant, item) {
-  if (restaurant.menus[item.type].includes(item)) return
+  if (restaurant.menus[item.type].includes(item)) return;
 
   restaurant.menus[item.type].push(item);
+}
+
+
+function removeMenuItem(restaurant, itemName, type) {
+  if (!itemExists(restaurant, itemName)) {
+    return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+  }
+  
+  let typeArr = restaurant.menus[type];
+  let item = typeArr.find(item => item.itemName === itemName);
+  let itemIdx = typeArr.indexOf(item);
+  
+  typeArr.splice(itemIdx, 1);
+  
+  return `No one is eating our ${itemName} - it has been removed from the ${type} menu!`;
+}
+
+function itemExists(restaurant, itemName) {
+  let menus = restaurant.menus;
+  let exists = false;
+  
+  for (type in menus) {
+    if (menus[type].find(item => item.name === itemName)) {
+      exists = true;
+    }
+  }
+
+  return exists;
 }
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
