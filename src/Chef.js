19914b48fdfe49932a61f2690cb3d49be6832dd8
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-05 14:52:35 -0400

    Pass Chef.js tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,30 @@
 class Chef {
-  constructor() {
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(customerName, morning=false) {
+    let greeting = morning ? `Good morning, ${customerName}!` : `Hello, ${customerName}!`
+    return greeting;
+  }
+
+  checkForFood(foodItem) {
+    let menus = this.restaurant.menus;
+    let onMenu = false;
+
+    for (let type in menus) {      
+      if (menus[type].includes(foodItem)) {
+        onMenu = true;
+        break;
+      }
+    }
     
+    if (onMenu) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
   }
 }
 
